6772709daf16eba4484cbb48864408286631596b
samnam298 <samrawithailena@gmail.com>
2025-10-21 11:20:59 -0400

    addItem(Product product, int qty)

--- src/main/java/com/pluralsight/Cart.java
+++ src/main/java/com/pluralsight/Cart.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
-}
+    private List<CartItem> items = new ArrayList<>();
+
+    public void addItem(Product product, int qty) {
+        for (CartItem item : items) {
+            if (item.getProduct().getSku().equalsIgnoreCase(product.getSku())) {
+                item.addQuantity(qty);
+                return;
+            }
+        }
+        items.add(new CartItem(product, qty));
+    }
