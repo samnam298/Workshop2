f0821aef03a8376b4de359e62fa50a242da9002f
samnam298 <samrawithailena@gmail.com>
2025-10-21 11:07:53 -0400

    Represents a single product from the store’s inventory

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
 public class Product {
+    private String sku;
+    private String name;
+    private double price;
+    private String department;
+
+    public Product(String sku, String name, double price, String department) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+        this.department = department;
+    }
+
+    public String getSku() { return sku; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public String getDepartment() { return department; }
+
+    @Override
+    public String toString() {
+        return String.format("%-8s | %-25s | $%-8.2f | %-15s", sku, name, price, department);
+    }
 }
