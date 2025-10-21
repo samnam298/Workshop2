c6e02dee0a291172b8a077be8b3428c1dc804611
samnam298 <samrawithailena@gmail.com>
2025-10-21 11:16:45 -0400

    each Product connects to its quantity and total price inside the cart.

--- src/main/java/com/pluralsight/CartItem.java
+++ src/main/java/com/pluralsight/CartItem.java
@@ -2,5 +2,22 @@ package com.pluralsight;
 
 public class CartItem {
     private Product product;
-    private int quantity;}
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() { return product; }
+    public int getQuantity() { return quantity; }
+    public void addQuantity(int amount) { this.quantity += amount; }
+    public double getTotal() { return product.getPrice() * quantity; }
+
+    @Override
+    public String toString() {
+        return String.format("%-25s x%-3d $%-8.2f", product.getName(), quantity, getTotal());
+    }
+}
+
 
